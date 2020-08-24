2bba7d5176ca25fd5befe7eef7e7e9bbb74a5df4
mattlennon3 <mattlennon3@hotmail.co.uk>
2020-08-24 23:24:54 +0100

    added easier (ish) logger

---
+++ log.lua
@@ -0,0 +1,13 @@
+local awful = require("awful")
+local logger = {}
+
+-- Log to a file
+function logger.log(message)
+    awful.spawn.with_shell(string.format("echo %s >> $HOME/Logs/awesome/awesome.log", message), false)
+end
+
+function logger.clearlog()
+    awful.spawn.with_shell("echo > $HOME/Logs/awesome/awesome.log", false)
+end
+
+return logger
