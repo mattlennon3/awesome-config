f8bbfbe00a483d205a9da36638fb906dfe5b17a1
mattlennon3 <mattlennon3@hotmail.co.uk>
2020-10-15 18:24:37 +0100

    Using rsync instead of system copy

--- update_awesome_config.sh
+++ update_awesome_config.sh
@@ -5,7 +5,7 @@ BACKUP_LOCATION="$HOME/.config/awesome_backup"
 AWESOME_CONFIG_GIT_LOCATION="$HOME/git/my-dotfiles/awesome-config"
 
 echo "Backing up current awesome config to $BACKUP_LOCATION"
-cp -r $CONFIG_LOCATION      $BACKUP_LOCATION || echo "error backing up the awesome config"
+rsync -au "$CONFIG_LOCATION/" $BACKUP_LOCATION --exclude=.git/ || echo "error backing up the awesome config"
 echo "Copying config from $AWESOME_CONFIG_GIT_LOCATION to $CONFIG_LOCATION"
-cp -r $AWESOME_CONFIG_GIT_LOCATION      $CONFIG_LOCATION
+rsync -au "$AWESOME_CONFIG_GIT_LOCATION/" $CONFIG_LOCATION
 echo "Done"
