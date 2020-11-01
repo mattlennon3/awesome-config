d084eb7ebad2883f9604812476f6898015bef8f3
mattlennon3 <mattlennon3@hotmail.co.uk>
2020-11-01 21:26:53 +0000

    Also logging to the console as well as a file

--- log.lua
+++ log.lua
@@ -4,6 +4,7 @@ local logger = {}
 -- Log to a file
 function logger.log(message)
     awful.spawn.with_shell(string.format("echo %s >> $HOME/Logs/awesome/awesome.log", message), false)
+    print(message)
 end
 
 function logger.clearlog()
